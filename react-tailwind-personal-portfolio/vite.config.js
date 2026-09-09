fd218e9f1dadc0ac0532be78fb6877806b7c9f36
Suhas-S2004 <suhas0168@gmail.com>
2026-09-10 01:23:12 +0530

    Changes

--- react-tailwind-personal-portfolio/vite.config.js
+++ react-tailwind-personal-portfolio/vite.config.js
@@ -11,4 +11,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: import.meta.env.VITE_BASE_PATH || "/react-tailwind-personal-portfolio-updated",
 });
